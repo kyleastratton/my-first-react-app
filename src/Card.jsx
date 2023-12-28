9a6bf553d5ba34fade486851cb6495325fe34c99
Kyle Stratton <kyle.stratton@icloud.com>
2023-12-28 18:15:33 +0000

    creating new file and playing with components

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+function Card() {
+  return (
+    <div className="container">
+      <h3>This is a heading</h3>
+      <input value="Input text..."></input>
+      <button className="primary-button">Click me</button>
+    </div>
+  )
+}
+
+export default Card
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import App from './App.jsx'
+import './style.css'
+// import Greeting from './Greeting.jsx'
+import Card from './Card.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className="main-container">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    {/* <App />
+    <Greeting /> */}
   </React.StrictMode>,
 )
