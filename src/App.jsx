0f876f2c942fb75cd66ecfc4e54253c77c36f49d
Kyle Stratton <kyle.stratton@icloud.com>
2024-02-03 15:47:41 +0000

    render cards from object

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,30 @@ class App extends React.Component {
           addressInput: "123 Main Street",
           mobileInput: "07912345678",
           bankInput: "Example Bank",
-          scInput: "10-20-30",
-          anInput: "00000001",
-          ibanInput: "GB29RBOS10203000000001",
+          sortCodeInput: "10-20-30",
+          accountNumberInput: "00000001",
+          ibaccountNumberInput: "GB29RBOS10203000000001",
           swiftInput: "RBOSGB2LXXX"
         }; 
     } 
   
     render() { 
+      const name = {headingText: "Name", inputText: this.state.nameInput};
+      const address = {headingText: "Address", inputText: this.state.addressInput};
+      const mobile = {headingText: "Mobile Number", inputText: this.state.mobileInut};
+      const bank = {headingText: "Bank", inputText: this.state.bankInput};
+      const sortcode = {headingText: "Sort Code", inputText: this.state.sortCodeInput};
+      const accountnumber = {headingText: "Acount Number", inputText: this.state.accountNumberInput};
+      const iban = {headingText: "IBAN", inputText: this.state.ibaccountNumberInput};
+      const swiftcode = {headingText: "SWIFT Code", inputText: this.state.swiftInput};
+      const card = [name, address, mobile, bank, sortcode, accountnumber, iban, swiftcode];
         return ( 
           <div className="main-container">
-            <Card headingText={"Name"} inputText={this.state.nameInput}/>
-            <Card headingText={"Address"} inputText={this.state.addressInput}/>
-            <Card headingText={"Mobile Number"} inputText={this.state.mobileInput}/>
-            <Card headingText={"Bank"} inputText={this.state.bankInput}/>
-            <Card headingText={"Sort Code"} inputText={this.state.scInput}/>
-            <Card headingText={"Account Number"} inputText={this.state.anInput}/>
-            <Card headingText={"IBAN"} inputText={this.state.ibanInput}/>
-            <Card headingText={"SWIFT Code"} inputText={this.state.swiftInput}/>
+            <>
+            {card.map(card => (
+              <Card headingText={card.headingText} inputText={card.inputText}/>
+            ))}
+            </>
        </div>
         ) 
     } 
